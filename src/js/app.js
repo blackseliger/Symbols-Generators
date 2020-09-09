c3108faf585fb7f34edff021ed6f1f7d828df7ae
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-09-10 01:59:46 +0300

    make full task, add test

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,46 @@
 // TODO: write your code here
-import sum from './basic';
+export default class Team {
+  constructor() {
+    this.heroes = [
+      {
+        name: 'Лучник',
+        type: 'Bowman',
+        health: 50,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
+      {
+        name: 'Маг',
+        type: 'Magician',
+        health: 50,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
+      {
+        name: 'Мечник',
+        type: 'Swordman',
+        health: 50,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
+    ];
+  }
 
-console.log('worked');
+  * [Symbol.iterator]() {
+    let index = 0;
+    const team = this.heroes;
+    while (index < team.length) {
+      yield this.heroes[index].type;
+      index += 1;
+    }
+  }
+}
 
-console.log(sum([1, 2]));
+const test1 = new Team();
+for (let type of test1) {
+  console.log(type)
+}
+
